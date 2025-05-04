1df4c47efc8c6ed6f2a659856de145ce89303f67
Haxwell <thehaxwell@proton.me>
2025-05-04 19:52:55 +0200

    made RectNodeFields rely on @motion-canvas/2d NodeProps to increase the range of possibility

---
+++ .changeset/curly-lemons-move.md
@@ -0,0 +1,5 @@
+---
+"@to-motion-canvas/utilities": patch
+---
+
+made RectNodeFields rely on @motion-canvas/2d NodeProps to increase the range of possibility"
--- packages/utilities/src/motionCanvasNodeTreeFields/nodeFields/RectNodeFields.ts
+++ packages/utilities/src/motionCanvasNodeTreeFields/nodeFields/RectNodeFields.ts
@@ -1,16 +1,40 @@
+import { PossibleSpacing, PossibleVector2, SignalValue } from '@motion-canvas/core';
 import { Position } from '../../Position';
 import { NumericalExpression } from '../../numericalExpression/NumericalExpression';
 import { NodeFields, NodeFieldsWithChildType } from './NodeFields';
+import { Length, LengthLimit, RectProps } from '@motion-canvas/2d';
+
+type MyLength = NumericalExpression | `${number}%`;
+
+type ChangeFieldTypesAdvanced<T> = {
+  [K in keyof T]: T[K] extends SignalValue<infer U>
+  ? (U extends PossibleSpacing
+    ? NumericalExpression
+    : U extends number
+    ? NumericalExpression
+    : U extends Length
+    ? MyLength
+    : U extends LengthLimit
+    ? MyLength | null | 'max-content' | 'min-content'
+    : U extends PossibleVector2
+    ? Position<NumericalExpression>
+    : U extends PossibleVector2<Length>
+    ? Position<NumericalExpression>
+    : U)
+  : T[K];
+};
 
 export interface RectNodeFieldsWithChildType<T>
-  extends NodeFieldsWithChildType<T> {
-  width?: NumericalExpression;
-  height?: NumericalExpression;
-  topLeft?: Position<NumericalExpression>;
-  fill?: string;
-  stroke?: string;
-  lineWidth?: NumericalExpression;
-  radius?: NumericalExpression;
+  //extends NodeFieldsWithChildType<T> {
+  //width?: NumericalExpression;
+  //height?: NumericalExpression;
+  //topLeft?: Position<NumericalExpression>;
+  //fill?: string;
+  //stroke?: string;
+  //lineWidth?: NumericalExpression;
+  //radius?: NumericalExpression;
+
+  extends NodeFieldsWithChildType<T>, ChangeFieldTypesAdvanced<Omit<RectProps, "children" | "ref">> {
 }
 
 export interface RectNodeFields
